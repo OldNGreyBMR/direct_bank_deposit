29f0062e7b30d0fd60c218356516b7929d5dfcfd
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2023-03-26 08:32:27 +1000

    v1.5.5.01
    Updated with AUS changes; removed cheques and moneyorders

--- NZ/includes/languages/english/modules/payment/dirbanknz.php
+++ NZ/includes/languages/english/modules/payment/dirbanknz.php
@@ -1,29 +1,41 @@
 <?php
 /*
-* Copyright (c) 2003-2023 The zen-cart developers 
-* Portions Copyright (c) 2003 osCommerce  
-* This source file is subject to version 2.0 of the GPL license, 
 * $Id: DIRBANKNZ.php  2010-6-22 06:57:21Z Nigel Thomson - adjusted from Crystal Jones code $
-* $Id: DIRBANKNZ.php 2023-02-19 OldNGrey (BMH) for zc157d and PHP7.4 (NOT for PHP7.3)
+* v1.5.5.01 $Id: dirbank.php OldNGrey (BMH)) 2023-03-26 dirbanknz.php for zc157
+* 2023-03-26 removed cheques and moneyorders
 */
+$ln=$_SESSION['customer_last_name'] ?? '';    // required for inclusion below
+$id=$_SESSION['customer_id'] ?? '';
+
+define ('MODULE_PAYMENT_DIRBANKNZ_PAYABLE', '');
+define ('MODULE_PAYMENT_DIRBANKNZ_ADDRESS', '');
 define ('MODULE_PAYMENT_DIRBANKNZ_TEXT_TITLE', 'Direct Bank NZ');
-define ('MODULE_PAYMENT_DIRBANKNZ_TEXT_DESCRIPTION','Direct Bank Transfer');
+define ('MODULE_PAYMENT_DIRBANKNZ_DESCRIPTION','Direct Bank Transfer NZ');
 
-if (defined('MODULE_PAYMENT_DIRBANKNZ_STATUS') && MODULE_PAYMENT_DIRBANKNZ_STATUS == 1) {
-  define('MODULE_PAYMENT_DIRBANKNZ_TEXT_EMAIL_FOOTER', 
-  "Please use the following details to transfer your total order value:\n\n" .
-  "\nAccount No.:   " . MODULE_PAYMENT_DIRBANKNZ_ACCNUM .
-  "\nAccount Name:  " . MODULE_PAYMENT_DIRBANKNZ_ACCNAM . 
-  "\nBank Name:     " . MODULE_PAYMENT_DIRBANKNZ_BANKNAM .
-  "\n\nThanks for your order which will ship immediately once we receive payment in the above account.\n");
-}
+define('EMAIL_TEXT_NO_DELIVERY',   '<p>No Delivery, see below:');
 
-if (defined('MODULE_PAYMENT_DIRBANKNZ_STATUS') && MODULE_PAYMENT_DIRBANKNZ_STATUS == 1) {
-  define('MODULE_PAYMENT_DIRBANKNZ_TEXT_TITLE', 'Direct Bank Deposit - NZ');
-   define('MODULE_PAYMENT_DIRBANKNZ_TEXT_DESCRIPTION', 
-  '<BR>Please use the following details to transfer your total order value:<br><pre>' . 
-  "\nAccount No.:    " . MODULE_PAYMENT_DIRBANKNZ_ACCNUM .
-  "\nAccount Name:   " . MODULE_PAYMENT_DIRBANKNZ_ACCNAM . 
-  "\nBank Name:      " . MODULE_PAYMENT_DIRBANKNZ_BANKNAM .
-  '</pre><p>Thanks for your order which will ship immediately once we receive payment in the above account.');
+if (defined('MODULE_PAYMENT_DIRBANKNZ_STATUS')  {
+    define('MODULE_PAYMENT_DIRBANKNZ_TEXT_EMAIL_FOOTER',
+        "Please use the following details to transfer your total order value:\n\n" .
+        "\nAccount No.:   " . MODULE_PAYMENT_DIRBANKNZ_ACCNUM .
+        "\nAccount Name:  " . MODULE_PAYMENT_DIRBANKNZ_ACCNAM .
+        "\nBank Name:     " . MODULE_PAYMENT_DIRBANKNZ_BANKNAM .
+        "\n\nThanks for your order which will ship immediately once we receive payment in the above account.\n" );
+
+    define ('MODULE_PAYMENT_DIRBANKNZ_HTML_EMAIL_FOOTER',
+        '<BR>Please use the following details to transfer your total order value:<br><pre>' .
+        "\nAccount No.:  " . MODULE_PAYMENT_DIRBANKNZ_ACCNUM .
+        "\nAccount Name: " . MODULE_PAYMENT_DIRBANKNZ_ACCNAM .
+        "\nBank Name:    " . MODULE_PAYMENT_DIRBANKAUS_BANKNAM .
+        '</pre><p>Thanks for your order which will ship immediately once we receive payment in the above account.');
+
+    //  insert details on payment screen //
+    define ('MODULE_PAYMENT_DIRBANKNZ_TEXT_DESCRIPTION',
+        '<br>Banking and Address details will also be sent to your email once the order is confirmed.<br><pre>'  .
+       '<br>Please use the following details to transfer your total order value:<br><pre>' .
+        "\nAccount No.:  " . MODULE_PAYMENT_DIRBANKNZ_ACCNUM .
+        "\nAccount Name: " . MODULE_PAYMENT_DIRBANKNZ_ACCNAM .
+        "\nBank Name:    " . MODULE_PAYMENT_DIRBANKNZ_BANKNAM .
+        "\nReference:    "  . $ln ."-" . $id . "-%s" .
+        '</pre><p>Thanks for your order which will ship immediately <br>once we receive payment in the above account." ') ;
 }
--- NZ/includes/languages/english/modules/payment/lang.dirbanknz.php
+++ NZ/includes/languages/english/modules/payment/lang.dirbanknz.php
@@ -1,10 +1,10 @@
 <?php
-// DIRBANKNZ based on  $Id: DIRBANKNZ.php 1970 2010-6-22 06:57:21Z Nigel Thomson - adjusted from Crystal Jones code $
-// BMH (OldNGrey) 2023-02-19 lang.dirbanknz.php for zc158
-//                changes based on working version of DIRBANKAUS
-// DIRBANKNZ V1.5.5 2023-02-19
+// DIRBANKNZ based on  $Id: DIRBANKNZZ.php 1970 2010-6-22 06:57:21Z Nigel Thomson - adjusted from Crystal Jones code $
+// v1.5.5.01 $Id: lang.dirbank.php OldNGrey (BMH)) 2023-03-26 lang.dirbanknz.php for zc158
+// 2023-03-26 removed cheques and moneyorders
 
 $define = [
+    //'MODULE_PAYMENT_DIRBANKNZ_TEXT_TITLE' => 'Direct Bank NZ',
     'MODULE_PAYMENT_DIRBANKNZ_TEXT_DESCRIPTION' => 'Direct Bank Transfer',
     'EMAIL_TEXT_NO_DELIVERY' => '<p>No Delivery, see below:',
     'MODULE_PAYMENT_DIRBANKNZ_TEXT_TITLE' => 'Direct-Bank-Deposit - New Zealand',
@@ -15,6 +15,8 @@ $define = [
 $ln=$_SESSION['customer_last_name'] ?? '';    // required for inclusion below
 $id=$_SESSION['customer_id'] ?? '';           // required for inclusion below
 
+
+//if (defined('MODULE_PAYMENT_DIRBANKNZ_STATUS') && MODULE_PAYMENT_DIRBANKNZ_STATUS == 'True') {
 if (defined('MODULE_PAYMENT_DIRBANKNZ_STATUS')) {
        // BMH insert details on payment screen //
     $define['MODULE_PAYMENT_DIRBANKNZ_TEXT_DESCRIPTION'] = '<br>Banking and Address details will also be sent to your email once the order is confirmed.<br><pre>' .
@@ -23,7 +25,7 @@ if (defined('MODULE_PAYMENT_DIRBANKNZ_STATUS')) {
         "\nAccount Name: " . MODULE_PAYMENT_DIRBANKNZ_ACCNAM .
         "\nBank Name:    " . MODULE_PAYMENT_DIRBANKNZ_BANKNAM .
         "\nReference:    "  . $ln ."-" . $id . "-%s" .
-        '</pre><p>Thanks for your order which will ship immediately <br>once we receive payment in the above account.' 
+        '</pre><p>Thanks for your order which will ship immediately <br>once we receive payment in the above account.' // BMH //
         ;
 
   $define ['MODULE_PAYMENT_DIRBANKNZ_TEXT_EMAIL_FOOTER'] = "Please use the following details to transfer your total order value:\n\n" .
@@ -31,8 +33,6 @@ if (defined('MODULE_PAYMENT_DIRBANKNZ_STATUS')) {
         "\nAccount Name: " . MODULE_PAYMENT_DIRBANKNZ_ACCNAM .
         "\nBank Name:    " . MODULE_PAYMENT_DIRBANKNZ_BANKNAM .
         "\nReference:    "  . $ln ."-" . $id . "-%s" .
-        "\n\nSend Cheques/Money Orders To:    " . defined('MODULE_PAYMENT_DIRBANKNZ_ADDRESS')  .
-        "\nCheques/Money Orders Payable To:   " . defined('MODULE_PAYMENT_DIRBANKNZ_PAYABLE') .
         "\n\nThanks for your order which will ship immediately once we receive payment in the above account.\n"
   ;
 
@@ -41,11 +41,9 @@ if (defined('MODULE_PAYMENT_DIRBANKNZ_STATUS')) {
         "\nAccount Name: " . MODULE_PAYMENT_DIRBANKNZ_ACCNAM .
         "\nBank Name:    " . MODULE_PAYMENT_DIRBANKNZ_BANKNAM .
         "\nReference:    "  . $ln ."-" . $id . "-%s" .
-        "\n\nSend Cheques/Money Orders To:    " . defined('MODULE_PAYMENT_DIRBANKNZ_ADDRESS') .
-        "\nCheques/Money Orders Payable To:   " . defined('MODULE_PAYMENT_DIRBANKNZ_PAYABLE') .
         '</pre><p>Thanks for your order which will ship immediately once we receive payment in the above account.'
         ;
     }
 
 return $define;
-
+?>
