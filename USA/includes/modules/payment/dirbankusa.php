853bb8c46d16f24f393f3d323a4b4e3a1f0a1cb5
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2024-02-14 12:00:26 +1000

    Add files via upload
    v1.5.8

--- USA/includes/modules/payment/dirbankusa.php
+++ USA/includes/modules/payment/dirbankusa.php
@@ -1,47 +1,52 @@
 <?php
 /*
-* Copyright (c) 2003-2023 The zen-cart developers
-* Portions Copyright (c) 2003 osCommerce 
+* Copyright (c) 2003-2024 The zen-cart developers
+* Portions Copyright (c) 2003 osCommerce
 * * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
 * $Id: DIRBANKUSA.php 1106 2009-11-24 22:05:35Z CRYSTAL JONES $ modify from Auzbank of OZcommerce module by birdbrain
-* @version $Id DIRBANKAUS V2.5.5 2023-02-19 BMH for zc158 PHP8.1 + PHP8.2
+* @version $Id DIRBANKUSA V158 2024-02-02 BMH for zc158 zc158a PHP8.2 PHP8.3
 */
-//BMH 2023-02-19 reconfigured for PHP7.4
+
 declare(strict_types = 1);
 
-$id=isset($_SESSION['customer_id']);
-$ln=isset($_SESSION['customer_last_name']);
+$id=isset($_SESSION['customer_id']);            // BMH @$id=$_SESSION['customer_id'];
+$ln=isset($_SESSION['customer_last_name']);     // BMH @$ln=$_SESSION['customer_last_name'];
 
-  class dirbankusa {
-    
-    public $code;       
-    public $description;        // $description is a soft name for this payment method @var string 
-    public $email_footer;       //$email_footer is the text to me placed in the footer of the email @var string
-    public $enabled;            //
-    public $order_status;       // $order_status is the order status to set after processing the payment
-    public $sort_order;         // $sort_order is the order priority of this payment module when displayed
-    public $title;              // $title is the displayed name for this order total method
-    public $check;              //
-    public $_check;             //   
+class dirbankusa extends base
+{
+    public $code;
+    public $description;                // $description is a soft name for this payment method @var string
+    public $email_footer;               // $email_footer is the text to me placed in the footer of the email @var string
+    public $enabled;                    // $enabled determines whether this module shows or not... in catalog.
+    public $moduleVersion = '1.5.8';    // $moduleVersion is the plugin version number
+    public $order_status;               // $order_status is the order status to set after processing the payment
+    public $sort_order;                 // $sort_order is the order priority of this payment module when displayed
+    public $title;                      // $title is the displayed name for this order total method
+    public $check;                      //
+    protected $_check;                  //
 
 // class constructor
-    function __construct() {
-      global $order;
+    public function __construct() {
+        global $order;
 
-      $this->code = 'dirbankusa';
-      $this->title = MODULE_PAYMENT_DIRBANKUSA_TEXT_TITLE;
-      $this->description = MODULE_PAYMENT_DIRBANKUSA_DESCRIPTION;
-      $this->email_footer = defined('MODULE_PAYMENT_DIRBANKUSA_TEXT_EMAIL_FOOTER');
-      $this->sort_order = defined('MODULE_PAYMENT_DIRBANKUSA_SORT_ORDER') ? MODULE_PAYMENT_DIRBANKUSA_SORT_ORDER : NULL;
-      $this->enabled = (defined('MODULE_PAYMENT_DIRBANKUSA_STATUS') && MODULE_PAYMENT_DIRBANKUSA_STATUS  == 'True') ;
-      
-if (!defined('MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID')) {define('MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID', '');}
+        $this->code = 'dirbankusa';
+        $this->enabled = (defined('MODULE_PAYMENT_DIRBANKUSA_STATUS') && MODULE_PAYMENT_DIRBANKUSA_STATUS  == 'True') ;
+        $this->sort_order = defined('MODULE_PAYMENT_DIRBANKUSA_SORT_ORDER') ? MODULE_PAYMENT_DIRBANKUSA_SORT_ORDER : null;
+        $this->title = MODULE_PAYMENT_DIRBANKUSA_TEXT_TITLE  ;                    // Payment module title in Catalog
+        if (IS_ADMIN_FLAG === true) {
+            $this->description = '<strong>' . MODULE_PAYMENT_DIRBANKUSA_TEXT_TITLE . ' ' . $this->moduleVersion . ' </strong>';
+            $this->description .= '<br><br>' .  MODULE_PAYMENT_DIRBANKUSA_TEXT_DESCRIPTION;
+        }
 
-      if ((int)MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID > 0) {
-        $this->order_status = MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID ;
-      }
+        $this->email_footer = defined('MODULE_PAYMENT_DIRBANKUSA_TEXT_EMAIL_FOOTER');
 
-      if (is_object($order)) $this->update_status();
+        if (null === $this->sort_order) return false;
+
+        if ((int)MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID > 0) {
+            $this->order_status = MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID;
+        }
+
+        if (is_object($order)) $this->update_status();
     }
 
 // class methods
@@ -107,9 +112,9 @@ if (!defined('MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID')) {define('MODULE_PAYME
       return false;
     }
 
-    function check() {
+    public function check() {
       global $db;
-      global $_check;
+      // global $_check;
       if (!isset($this->_check)) {
         $check_query = $db->Execute("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_DIRBANKUSA_STATUS'");
         $this->_check = $check_query->RecordCount();
@@ -117,24 +122,37 @@ if (!defined('MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID')) {define('MODULE_PAYME
       return $this->_check;
     }
 
-    function install() {
-      global $db;
-     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Direct Bank Deposit Module', 'MODULE_PAYMENT_DIRBANKUSA_STATUS', 'True', 'Do you want to accept USA Bank Deposit payments?', '6', '1', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
-	 $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Payment Zone', 'MODULE_PAYMENT_DIRBANKUSA_ZONE', '0', 'If a zone is selected, only enable this payment method for that zone.', '6', '2', 'zen_get_zone_class_title', 'zen_cfg_pull_down_zone_classes(', now())");
-     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort order of display.', 'MODULE_PAYMENT_DIRBANKUSA_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
-     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Routing Number', 'MODULE_PAYMENT_DIRBANKUSA_ROUTING', '000-000-000', '9 digit routing number in the format 000-000-000', '6', '1', now());");
-     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Bank Account No.', 'MODULE_PAYMENT_DIRBANKUSA_ACCNUM', '12345678', 'Bank Account No.', '6', '1', now());");
-     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Bank Account Name', 'MODULE_PAYMENT_DIRBANKUSA_ACCNAM', 'Joe Bloggs', 'Bank Account Name', '6', '1', now());");
-     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Bank Name', 'MODULE_PAYMENT_DIRBANKUSA_BANKNAM', 'The Bank', 'Bank Name', '6', '1', now());");
-     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, use_function, date_added) values ('Set Order Status', 'MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID', '0', 'Set the status of orders made with this payment module to this value', '6', '0', 'zen_cfg_pull_down_order_statuses(', 'zen_get_order_status_name', now())");
+    function install()
+    {
+        global $db;
+
+        if (!defined('MODULE_PAYMENT_DIRBANKUSA_STATUS')) $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Direct Bank Deposit Module', 'MODULE_PAYMENT_DIRBANKUSA_STATUS', 'True', 'Do you want to accept USA Bank Deposit payments?', '6', '1', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
+        if (!defined('MODULE_PAYMENT_DIRBANKUSA_ZONE')) $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Payment Zone', 'MODULE_PAYMENT_DIRBANKUSA_ZONE', '0', 'If a zone is selected, only enable this payment method for that zone.', '6', '2', 'zen_get_zone_class_title', 'zen_cfg_pull_down_zone_classes(', now())");
+        if (!defined('MODULE_PAYMENT_DIRBANKUSA_SORT_ORDER')) $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort order of display.', 'MODULE_PAYMENT_DIRBANKUSA_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
+        if (!defined('MODULE_PAYMENT_DIRBANKUSA_ROUTING')) $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Routing Number', 'MODULE_PAYMENT_DIRBANKUSA_ROUTING', '000-000-000', '9 digit routing number in the format 000-000-000', '6', '1', now());");
+        if (!defined('MODULE_PAYMENT_DIRBANKUSA_ACCNUM')) $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Bank Account No.', 'MODULE_PAYMENT_DIRBANKUSA_ACCNUM', '12345678', 'Bank Account No.', '6', '1', now());");
+        if (!defined('MODULE_PAYMENT_DIRBANKUSA_ACCNAM')) $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Bank Account Name', 'MODULE_PAYMENT_DIRBANKUSA_ACCNAM', 'Joe Bloggs', 'Bank Account Name', '6', '1', now());");
+        if (!defined('MODULE_PAYMENT_DIRBANKUSA_BANKNAM')) $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Bank Name', 'MODULE_PAYMENT_DIRBANKUSA_BANKNAM', 'The Bank', 'Bank Name', '6', '1', now());");
+        if (!defined('MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID')) $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, use_function, date_added) values ('Set Order Status', 'MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID', '0', 'Set the status of orders made with this payment module to this value', '6', '0', 'zen_cfg_pull_down_order_statuses(', 'zen_get_order_status_name', now())");
    }
 
-    function remove() {
-      global $db;
-      $db->Execute("delete from " . TABLE_CONFIGURATION . " where configuration_key in ('" . implode("', '", $this->keys()) . "')");
+    public function remove()
+    {
+        global $db;
+        $db->Execute("delete from " . TABLE_CONFIGURATION . " where configuration_key in ('" . implode("', '", $this->keys()) . "')");
     }
 
-    function keys() {
-	  return array('MODULE_PAYMENT_DIRBANKUSA_STATUS', 'MODULE_PAYMENT_DIRBANKUSA_ZONE', 'MODULE_PAYMENT_DIRBANKUSA_SORT_ORDER', 'MODULE_PAYMENT_DIRBANKUSA_ROUTING', 'MODULE_PAYMENT_DIRBANKUSA_ACCNUM', 'MODULE_PAYMENT_DIRBANKUSA_ACCNAM',  'MODULE_PAYMENT_DIRBANKUSA_BANKNAM', 'MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID');
+    public function keys()
+    {
+        return array(
+            'MODULE_PAYMENT_DIRBANKUSA_STATUS',
+            'MODULE_PAYMENT_DIRBANKUSA_ZONE',
+            'MODULE_PAYMENT_DIRBANKUSA_SORT_ORDER',
+            'MODULE_PAYMENT_DIRBANKUSA_ROUTING',
+            'MODULE_PAYMENT_DIRBANKUSA_ACCNUM',
+            'MODULE_PAYMENT_DIRBANKUSA_ACCNAM',
+            'MODULE_PAYMENT_DIRBANKUSA_BANKNAM',
+            'MODULE_PAYMENT_DIRBANKUSA_ORDER_STATUS_ID',
+            );
     }
   }
