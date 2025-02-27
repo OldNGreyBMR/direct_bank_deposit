6623c1fed23d4a1eafc0e5a764432bcec8dec4fa
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2025-02-27 13:30:59 +1000

    Add files via upload

--- USA/includes/languages/english/modules/payment/lang.dirbankusa.php
+++ USA/includes/languages/english/modules/payment/lang.dirbankusa.php
@@ -1,38 +1,56 @@
 <?php
-// $Id: DIRBANKUSA.php 1970 2009-11-24 06:57:21Z CRYSTAL JONES $
-// BMR (OldNGrey) v158 2024-02-02 lang.dirbankusa.php for zc158
+// Copyright (c) 2024-2025 The zen-cart developers                           |
+// BMH (OldNGrey) v2.1.1 2025-02-26 lang.dirbankusa.php for zc158a to zc210 PHP8.2 to PHP8.4
+//
 
+$define = [
+    'MODULE_PAYMENT_DIRBANKUSA_TEXT_DESCRIPTION' => 'Direct Bank Transfer USA',
+    'EMAIL_TEXT_NO_DELIVERY' => '<p>No Delivery, see below:', 
+    'MODULE_PAYMENT_DIRBANKUSA_TEXT_TITLE' => 'Direct-Bank-Deposit - USA',
+    'MODULE_PAYMENT_DIRBANKUSA_ADDRESS' => '',
+    'MODULE_PAYMENT_DIRBANKUSA_PAYABLE' => '', 
+];
 $ln=$_SESSION['customer_last_name'] ?? '';    // required for inclusion below
 $id=$_SESSION['customer_id'] ?? '';           // required for inclusion below
 
-$define = [
-    'EMAIL_TEXT_NO_DELIVERY' => '<p>No Delivery, see below:',
-    'MODULE_PAYMENT_DIRBANKUSA_TEXT_TITLE' => 'Direct-Bank-Deposit - USA',
-    'MODULE_PAYMENT_DIRBANKUSA_TEXT_DESCRIPTION' => 'Banking and Address details will also be sent to your email once the order is confirmed.<pre>' .
-        'Please use the following details to transfer <br>your total order value:' .
-        "\nAccount No.:  " .  (defined('MODULE_PAYMENT_DIRBANKUSA_ACCNUM')  ? MODULE_PAYMENT_DIRBANKUSA_ACCNUM : '(your Ac num)' ).
-        "\nRouting Number:" . (defined('MODULE_PAYMENT_DIRBANKUSA_ROUTING') ? MODULE_PAYMENT_DIRBANKUSA_ROUTING : '(your Routing num)' ).
-        "\nAccount Name: " .  (defined('MODULE_PAYMENT_DIRBANKUSA_ACCNAM')  ? MODULE_PAYMENT_DIRBANKUSA_ACCNAM : '(your Ac name)' ) .
-        "\nBank Name:    " .  (defined('MODULE_PAYMENT_DIRBANKUSA_BANKNAM') ? MODULE_PAYMENT_DIRBANKUSA_BANKNAM : '(your bank name)' ) .
-        "\nReference:    "  . $ln ."-" . $id . "-%s" .
-        '</pre>Thanks for your order which will ship immediately <br>once we receive payment in the above account.'
-        ,
-    'MODULE_PAYMENT_DIRBANKUSA_TEXT_EMAIL_FOOTER' => "Please use the following details to transfer <br>your total order value:\n" .
-        "\nAccount No.:  "  . (defined('MODULE_PAYMENT_DIRBANKUSA_ACCNUM')  ? MODULE_PAYMENT_DIRBANKUSA_ACCNUM : '(your Ac num)' ).
-        "\nRouting Number:" . (defined('MODULE_PAYMENT_DIRBANKUSA_ROUTING') ? MODULE_PAYMENT_DIRBANKUSA_ROUTING : '(your Routing num)' ).
-        "\nAccount Name: "  . (defined('MODULE_PAYMENT_DIRBANKUSA_ACCNAM')  ? MODULE_PAYMENT_DIRBANKUSA_ACCNAM : '(your Ac name)' ) .
-        "\nBank Name:    "  . (defined('MODULE_PAYMENT_DIRBANKUSA_BANKNAM') ? MODULE_PAYMENT_DIRBANKUSA_BANKNAM : '(your bank name)' ) .
-        "\nReference:    "  . $ln ."-" . $id . "-%s" .
-        "\nThanks for your order which will ship immediately once we receive payment in the above account.\n"
-        ,
-    'MODULE_PAYMENT_DIRBANKUSA_HTML_EMAIL_FOOTER' => '<br>Please use the following details to transfer <br>your total order value:<pre>' .
-        "\nAccount No.:  "  .(defined('MODULE_PAYMENT_DIRBANKUSA_ACCNUM')  ? MODULE_PAYMENT_DIRBANKUSA_ACCNUM : '(your Ac num)' ).
-        "\nRouting Number:" .(defined('MODULE_PAYMENT_DIRBANKUSA_ROUTING') ? MODULE_PAYMENT_DIRBANKUSA_ROUTING : '(your Routing num)' ).
-        "\nAccount Name: "  .(defined('MODULE_PAYMENT_DIRBANKUSA_ACCNAM')  ? MODULE_PAYMENT_DIRBANKUSA_ACCNAM : '(your Ac name)' ) .
-        "\nBank Name:    "  .(defined('MODULE_PAYMENT_DIRBANKUSA_BANKNAM') ? MODULE_PAYMENT_DIRBANKUSA_BANKNAM : '(your bank name)' ) .
-        "\nReference:    "  . $ln ."-" . $id . "-%s" .
-        '</pre><br>Thanks for your order which will ship immediately once we receive payment in the above account.'
-        ,
-];
 
+if (defined('MODULE_PAYMENT_DIRBANKUSA_STATUS') && 
+    (defined('MODULE_PAYMENT_DIRBANKUSA_STATUS') == 'True') ) {
+       //  insert details on payment screen //
+    $define['MODULE_PAYMENT_DIRBANKUSA_TEXT_DESCRIPTION'] = 'Banking and Address details will also be sent to your email once the order is confirmed.<br>' . 
+        '<br>Please use the following details to transfer your total order value:<br><pre>' . 
+        "\nAccount No.:  " . MODULE_PAYMENT_DIRBANKUSA_ACCNUM .
+        "\nRouting Number:" . MODULE_PAYMENT_DIRBANKUSA_ROUTING .
+        "\nAccount Name: " . MODULE_PAYMENT_DIRBANKUSA_ACCNAM . 
+        "\nBank Name:    " . MODULE_PAYMENT_DIRBANKUSA_BANKNAM .
+        "\nReference:    "  . $ln ."-" . $id . "-%s" .
+        '<p>Thanks for your order which will ship immediately <br>once we receive payment in the above account.' 
+        ; 
+        
+  $define ['MODULE_PAYMENT_DIRBANKUSA_TEXT_EMAIL_FOOTER'] = "Please use the following details to transfer your total order value:\n\n" .
+        "\nAccount No.:  " . MODULE_PAYMENT_DIRBANKUSA_ACCNUM .
+        "\nRouting Number:" . MODULE_PAYMENT_DIRBANKUSA_ROUTING .
+        "\nAccount Name: " . MODULE_PAYMENT_DIRBANKUSA_ACCNAM . 
+        "\nBank Name:    " . MODULE_PAYMENT_DIRBANKUSA_BANKNAM .
+        "\nReference:    "  . $ln ."-" . $id . "-%s" .
+        "\n\nSend Cheques/Money Orders To:    " . defined('MODULE_PAYMENT_DIRBANKUSA_ADDRESS')  . 
+        "\nCheques/Money Orders Payable To:   " . defined('MODULE_PAYMENT_DIRBANKUSA_PAYABLE') .
+        "\n\nThanks for your order which will ship immediately once we receive payment in the above account.\n" 
+  ; 
+
+    $define ['MODULE_PAYMENT_DIRBANKUSA_HTML_EMAIL_FOOTER'] = '<br>Please use the following details to transfer your total order value:<br>' .
+        "\nAccount No.:  " . MODULE_PAYMENT_DIRBANKUSA_ACCNUM .
+        "\nRouting Number:" . MODULE_PAYMENT_DIRBANKUSA_ROUTING .
+        "\nAccount Name: " . MODULE_PAYMENT_DIRBANKUSA_ACCNAM . 
+        "\nBank Name:    " . MODULE_PAYMENT_DIRBANKUSA_BANKNAM .
+        "\nReference:    "  . $ln ."-" . $id . "-%s" .
+        "\n\nSend Cheques/Money Orders To:    " . defined('MODULE_PAYMENT_DIRBANKUSA_ADDRESS') . 
+        "\nCheques/Money Orders Payable To:   " . defined('MODULE_PAYMENT_DIRBANKUSA_PAYABLE') .
+        '<p>Thanks for your order which will ship immediately once we receive payment in the above account.' 
+        ;
+    }else {
+        $define['MODULE_PAYMENT_DIRBANKUSA_TEXT_DESCRIPTION'] = '<br>Setup DirbankUSA';
+        $define['MODULE_PAYMENT_DIRBANKUSA_TEXT_EMAIL_FOOTER'] = 'Please Setup DirbankUSA';
+        $define['MODULE_PAYMENT_DIRBANKUSA_HTML_EMAIL_FOOTER']  = '<br>Please Setup DirbankUSA';
+    }
 return $define;
