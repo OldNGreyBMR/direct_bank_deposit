455d7b0726fc47e13d76d891011f76cc087e13eb
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2025-12-20 13:41:06 +1000

    Add files via upload

---
+++ ZA/zc_plugins/DirectBankDepositZA/V2.1.2/catalog/includes/languages/english/modules/payment/lang.dirbankza.php
@@ -0,0 +1,51 @@
+<?php
+// DIRBANKZA based on  $Id: DIRBANKNZA.php 1970 2010-6-22 06:57:21Z Nigel Thomson - adjusted from Crystal Jones code $
+// BMH (OldNGrey) v2.1.2 2025-12-20 lang.dirbankza.php for zc220 PHP 8.2 to PHP 8.4
+
+$define = [
+    'MODULE_PAYMENT_DIRBANKZA_TEXT_DESCRIPTION' => 'Direct Bank Transfer - ZA',
+    'EMAIL_TEXT_NO_DELIVERY' => '<p>No Delivery, see below:',
+    'MODULE_PAYMENT_DIRBANKZA_TEXT_TITLE' => 'Direct Bank Deposit - Sth Africa',
+    'MODULE_PAYMENT_DIRBANKZA_ADDRESS' => '',
+    'MODULE_PAYMENT_DIRBANKZA_PAYABLE' => '',
+];
+
+$ln=$_SESSION['customer_last_name'] ?? '';    // required for inclusion below
+$id=$_SESSION['customer_id'] ?? '';           // required for inclusion below
+
+if (defined('MODULE_PAYMENT_DIRBANKZA_STATUS') &&
+        (defined('MODULE_PAYMENT_DIRBANKZA_STATUS') == 'True') ) {          //  insert details on payment screen //
+    $define['MODULE_PAYMENT_DIRBANKZA_TEXT_DESCRIPTION'] = 'Banking and Address details will also be sent to your email once the order is confirmed.<br>' .
+        '<br>Please use the following details to transfer your total order value:<br><pre>' .
+		"\nAccount Name:  " . defined('MODULE_PAYMENT_DIRBANKZA_ACCNAM') . 
+		"\nAccount No.:   " . defined('MODULE_PAYMENT_DIRBANKZA_ACCNUM') .
+        "\nBank Name:     " . defined('MODULE_PAYMENT_DIRBANKZA_BANKNAM') .
+        "\nBranch Code:   " . defined('MODULE_PAYMENT_DIRBANKZA_BANKCODE') .
+        "\nReference:      " . $ln ."-" . $id . "-%s" .
+        '<p>Thanks for your order which will ship immediately <br>once we receive payment in the above account. </pre>' // BMH //
+        ;
+
+  $define ['MODULE_PAYMENT_DIRBANKZA_TEXT_EMAIL_FOOTER'] = "Please use the following details to transfer your total order value:\n" .
+		"\nAccount Name:  " . defined('MODULE_PAYMENT_DIRBANKZA_ACCNAM') . 
+		"\nAccount No.:   " . defined('MODULE_PAYMENT_DIRBANKZA_ACCNUM') .
+        "\nBank Name:     " . defined('MODULE_PAYMENT_DIRBANKZA_BANKNAM') .
+        "\nBranch Code:   " . defined('MODULE_PAYMENT_DIRBANKZA_BANKCODE') .
+         "\nReference:     " . $ln ."-" . $id . "-%s" .
+        "\n\nThanks for your order which will ship immediately once we receive payment in the above account.\n"
+  ;
+
+    $define ['MODULE_PAYMENT_DIRBANKZA_HTML_EMAIL_FOOTER'] = '<br>Please use the following details to transfer your total order value:<br><em>' .
+		"\nAccount Name:  " . defined('MODULE_PAYMENT_DIRBANKZA_ACCNAM') . 
+		"\nAccount No.:   " . defined('MODULE_PAYMENT_DIRBANKZA_ACCNUM') .
+        "\nBank Name:     " . defined('MODULE_PAYMENT_DIRBANKZA_BANKNAM') .
+        "\nBranch Code:   " . defined('MODULE_PAYMENT_DIRBANKZA_BANKCODE') .
+        "\nReference:      " . $ln ."-" . $id . "-%s" .
+        '</em><p>Thanks for your order which will ship immediately once we receive payment in the above account.'
+        ;
+    }else {
+        $define['MODULE_PAYMENT_DIRBANKZA_TEXT_DESCRIPTION'] = '<br>Setup DirbankZA';
+        $define['MODULE_PAYMENT_DIRBANKZA_TEXT_EMAIL_FOOTER'] = 'Please Setup DirbankZA';
+        $define['MODULE_PAYMENT_DIRBANKZA_HTML_EMAIL_FOOTER']  = '<br>Please Setup DirbankZA';
+}
+
+return $define;
